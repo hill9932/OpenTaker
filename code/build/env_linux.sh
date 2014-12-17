aeacf0288f9d92d0e0a6240129c946717483776e
hill <hill9932@gmail.com>
2014-12-18 00:04:29 +0800

    compile packet ray

--- code/build/env_linux.sh
+++ code/build/env_linux.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+ln -s ../src/postgre-9.3/include 	../3rdParty/inc/postgre		
 ln -s /usr/include/boost/			../3rdParty/inc/boost
 ln -s /usr/include/gtest/			../3rdParty/inc/gtest
 ln -s /usr/include/lua5.2/			../3rdParty/inc/lua
-ln -s ../src/postgre-9.3/include 	../3rdParty/inc/postgre		
 ln -s /usr/include/popt				../3rdParty/inc/popt
+ln -s /usr/include/tbb				../3rdParty/inc/tbb
